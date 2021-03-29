cfa3d39ea171795affe6ef07a014341d9906b3e7
Maciej Byczko <252747@student.pwr.edu.pl>
2021-03-29 17:11:11 +0200

    Array created

--- array.cpp
+++ array.cpp
@@ -1,71 +1,120 @@
+#include <iostream>
 #include "array.h"
 
-class Array {
-    int *array; // pointer on first element
-    int size; // array length
 
-public:
-    /**
-     * Creating new Array
-     * @param givenArray
-     * @param size
-     */
-    Array(int *givenArray, int givenSize) : array(givenArray), size(givenSize) {
-        array = new int[givenSize];
-        size = givenSize;
-        for (int i = 0; i < size; ++i) {
-            array[i] = givenArray[i];
-        }
+Array::Array(const int *initArray, int mySize) {
+    array = new int[mySize];
+    size = mySize;
+    for (int i = 0; i < mySize; ++i) {
+        array[i] = initArray[i];
     }
-    /**
-     * destructor
-     */
-    virtual ~Array() {
-        delete[] array;
-    }
+}
 
-public:
-    /**
-     * add new element on front in array
-     * @param value
-     */
-    void addElementFront(int value) {
+Array::~Array() {
+    delete[] array;
+}
+
+/**
+ * add new element on front in array
+ * @param value
+ */
+void Array::addElementFront(int value) {
+    ++size;
+    int *newArray = new int[size];
+    newArray[0] = value;
+    for (unsigned int i = 1; i < size; ++i) {
+        newArray[i] = array[i - 1];
+    }
+    delete[] array;
+    array = newArray;
+}
+
+void Array::addElementBack(int value) {
+    int *newArray = new int[size + 1];
+    for (unsigned int i = 0; i < size; ++i) {
+        newArray[i] = array[i];
+    }
+    newArray[size] = value;
+    ++size;
+    delete[] array;
+    array = newArray;
+}
+
+void Array::addElementAnywhere(int value, unsigned int index) {
+    if (index <= size) {
         ++size;
         int *newArray = new int[size];
-        newArray[0] = value;
-        for (unsigned int i = 1; i < size; ++i) {
+        unsigned int i = 0;
+        for (; i < index; ++i) {
+            newArray[i] = array[i];
+        }
+        newArray[i] = value;
+        ++i;
+        for (; i < size; ++i) {
             newArray[i] = array[i - 1];
         }
         delete[] array;
         array = newArray;
     }
+}
 
-    void addElementBack(int value) {
-        int *newArray = new int[size + 1];
-        for (unsigned int i = 0; i < size; ++i) {
-            newArray[i] = array[i];
+void Array::removeElementFront() {
+    if (size > 0) {
+        --size;
+        int *newArray = new int[size];
+        for (int i = 0; i < size; ++i) {
+            newArray[i] = array[i + 1];
         }
-        newArray[size] = value;
-        ++size;
-        delete[] array;
+        delete array;
         array = newArray;
     }
+}
 
-    void addElementAnywhere(int value, unsigned int index) {
-        if (index <= size) {
-            ++size;
-            int *newArray = new int[size];
-            unsigned int i = 0;
-            for (; i < index; ++i) {
-                newArray[i] = array[i];
+void Array::removeElementBack() {
+    if (size > 0) {
+        --size;
+        int *newArray = new int[size];
+        for (int i = 0; i < size; ++i) {
+            newArray[i] = array[i];
+        }
+        delete array;
+        array = newArray;
+    }
+}
+
+void Array::removeElementAnywhere(unsigned int index) {
+    if (size > 0 && index <= size && index > 0) {
+        --size;
+        int *newArray = new int[size];
+        unsigned int i = 0;
+        for (i = 0; i < index; ++i) {
+            newArray[i] = array[i];
+        }
+        for (; i < size; ++i) {
+            newArray[i] = array[i];
+        }
+        delete array;
+        array = newArray;
+    }
+}
+
+void Array::removeValue(int value) {
+    removeElementAnywhere(findValue(value));
+}
+
+int Array::findValue(int value) {
+    if (size > 0) {
+        for (int i = 0; i < size; ++i) {
+            if (array[i] == value) {
+                return i;
             }
-            newArray[i] = value;
-            ++i;
-            for (; i < size; ++i) {
-                newArray[i] = array[i - 1];
-            }
-            delete[] array;
-            array = newArray;
         }
     }
-};
+    return -1;
+}
+
+void Array::display() {
+    for (int i = 0; i < size; ++i) {
+        std::cout << array[i];
+    }
+}
--- array.h
+++ array.h
@@ -1,1 +1,29 @@
 #pragma once
+
+class Array {
+    int *array; // pointer on first element
+    int size; // array length
+public:
+    Array(const int *initArray, int size);
+
+    virtual ~Array();
+
+public:
+    void addElementFront(int value);
+
+    void addElementBack(int value);
+
+    void addElementAnywhere(int value, unsigned int index);
+
+    void removeElementFront();
+
+    void removeElementBack();
+
+    void removeElementAnywhere(unsigned int index);
+
+    void removeValue(int value);
+
+    int findValue(int value);
+
+    void display();
+};
