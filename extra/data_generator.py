7390001c796cf5e181d4ff7ab2dc1fd6f8ab6114
Maciej Byczko <252747@student.pwr.edu.pl>
2021-04-17 14:38:35 +0200

    cleanup from additional files

---
+++ extra/data_generator.py
@@ -0,0 +1,12 @@
+import random
+
+
+def generate_data(quantity, file):
+    file.write(str(quantity)+"\n")
+    for element in range(0, quantity):
+        file.write(str(random.randint(-1000, 1000))+"\n")
+
+
+if __name__ == '__main__':
+    myFile = open("data.txt", "w")
+    generate_data(1000, myFile)
