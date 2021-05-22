392114f2976fea4ac630c797a3e5b34d1b620de9
Maciej Byczko <252747@student.pwr.edu.pl>
2021-05-22 08:48:53 +0200

    Created Matrix

--- SDiZO-Projekt_2/src/Matrix.cpp
+++ SDiZO-Projekt_2/src/Matrix.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
 #include "Matrix.h"
+
+using std::cout;
+
+Matrix::Matrix(int width, int height) {
+    sizeX = width;
+    sizeY = height;
+    array = new int *[sizeX];
+    for (int i = 0; i < sizeX; ++i) {
+        array[i] = new int[sizeY];
+        for (int j = 0; j < sizeY; ++j) {
+            array[i][j] = 0;
+        }
+    }
+}
+
+Matrix::~Matrix() {
+    for (int i = 0; i < sizeX; ++i) {
+        delete[] array[i];
+    }
+    delete[] array;
+}
+
+void Matrix::setValue(int x, int y, int value) {
+    array[x][y] = value;
+}
+
+int Matrix::getValue(int x, int y) {
+    return array[x][y];
+}
+
+void Matrix::display() {
+    for (int i = 0; i < sizeY; ++i) {
+        for (int j = 0; j < sizeX; ++j) {
+            cout<<"|"<<array[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
+    cout<<"\n";
+}
--- SDiZO-Projekt_2/src/Matrix.h
+++ SDiZO-Projekt_2/src/Matrix.h
@@ -1,4 +1,18 @@
 #pragma once
+
 class Matrix {
 
+    int **array;
+    int sizeX;
+    int sizeY;
+public:
+    Matrix(int width, int height);
+
+    virtual ~Matrix();
+
+    void setValue(int x, int y, int value);
+
+    int getValue(int x, int y);
+
+    void display();
 };
