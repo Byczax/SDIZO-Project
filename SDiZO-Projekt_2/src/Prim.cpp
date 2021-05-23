bd2ae2d2ea81a37c23279928bee7af6c2af1fd24
Maciej Byczko <252747@student.pwr.edu.pl>
2021-05-23 23:56:07 +0200

    starting prim algorithm
    Signed-off-by: Maciej Byczko <252747@student.pwr.edu.pl>

--- SDiZO-Projekt_2/src/Prim.cpp
+++ SDiZO-Projekt_2/src/Prim.cpp
@@ -1,5 +1,38 @@
 #include "Prim.h"
 
-void Prim::prim(int **graph) {
+PrimVertex::PrimVertex(int vertexNumber, int key) : vertexNumber(vertexNumber), key(key) {}
+
+void Prim::primMatrix(int *&key, int *&parent, int startingVertex, Matrix *graphMatrix) {
 
 }
+
+void Prim::primList(int *&key, int *&parent, int startingVertex, AdjacencyList *graphList) {
+    //stos wierzchołków Prima (tzn. obiektów wierzchołek posiadających swój numer, oraz key)
+//    auto *heap = new PrimVertexHeap(vertices);
+//    heap->primVertices[startingVertex]->setKey(0);
+//    key[startingVertex] = 0;
+//    parent[startingVertex] = -1;
+//    while (heap->hasElements()) {
+//        //tworzymy stos (aby mieć wierzchołek o najmniejszej wadze), trzeba co pętlę ponieważ w pętli zmieniają się elementy stosu
+//        heap->createMinHeap();
+//        PrimVertex *vertexU = heap->extractMin();
+//        //neighbourTraverse - obiekt (ListElement) za pomocą którego dobieramy się do wszystkich sąsiadów wierzchołka z listy sąsiedztwa
+//        auto neighbourTraverse = adjacencyLists[vertexU->getVertexNumber()]->getHead();
+//        while (neighbourTraverse != nullptr) {
+//            int neighbour = neighbourTraverse->neighbour;
+//            if (heap->isElementInHeap(neighbour)) {
+//                int edgeWeight = neighbourTraverse->edgeWeight;
+//                int neighbourPosition = heap->position[neighbour];
+//                if (edgeWeight < heap->primVertices[neighbourPosition]->getKey()) {
+//                    heap->primVertices[neighbourPosition]->setKey(edgeWeight);
+//                    key[neighbour] = edgeWeight;
+//                    parent[neighbour] = vertexU->getVertexNumber();
+//                }
+//            }
+//            neighbourTraverse = neighbourTraverse->next;
+//        }
+//    }
+//    delete heap;
+}
+
+
--- SDiZO-Projekt_2/src/Prim.h
+++ SDiZO-Projekt_2/src/Prim.h
@@ -1,6 +1,30 @@
 #pragma once
 
-class Prim {
+#include "Matrix.h"
+#include "List.h"
+
+class PrimVertex {
+
 public:
-    void prim(int **graph);
+    int vertexNumber;
+    int key;
+
+    PrimVertex(int vertexNumber, int key);
 };
+
+class Prim {
+
+    int heapSize;
+    int originalSize;
+
+    PrimVertex **primVertices;
+    int *position; // position[i] pokazuje gdzie w stosie jest wierzchołek 'i'
+public:
+    static void primMatrix(int *&key, int *&parent, int startingVertex, Matrix *graphMatrix);
+
+    static void primList(int *&key, int *&parent, int startingVertex, AdjacencyList *graphList);
+
+
+};
+
+
